86692259e169ea45f2a29cef3b1d652f5f939cab
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 22:00:30 +0530

    Update grab.py
    Signed-off-by: MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>

--- AnonXMusic/plugins/waifu/grab.py
+++ AnonXMusic/plugins/waifu/grab.py
@@ -9,8 +9,8 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pymongo import MongoClient
 
 
-from Sanatan.utils.database import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, Sanatan
-from config import application,  db, LOGGER
+from Sanatan.utils.database import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection,db 
+from config import application,  Sanatan, LOGGER
 from config import BOT_TOKEN, MONGO_DB_URI, SUPPORT_CHANNEL, SUPPORT_CHAT
 # Initialize MongoDB client
 client = MongoClient("MONGO_DB_URI")
