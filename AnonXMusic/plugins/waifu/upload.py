b0561009fc48f5630016fe5ba11980c0199a9347
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-05-01 22:18:40 +0530

    Update upload.py
    Signed-off-by: MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>

--- AnonXMusic/plugins/waifu/upload.py
+++ AnonXMusic/plugins/waifu/upload.py
@@ -4,7 +4,8 @@ from pymongo import ReturnDocument
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from Sanatan import application, sudo_users, collection, db
+from Sanatan import sudo_users, collection, db
+from AnonXMusic import application
 from config import LOGGER_ID, SUPPORT_CHAT
 
 WRONG_FORMAT_TEXT = """Wʀᴏɴɢ ❌ ғᴏʀᴍᴀᴛ...  ᴇɢ. /upload Iᴍɢ_ᴜʀʟ ᴍᴜᴢᴀɴ-ᴋɪʙᴜᴛsᴜɪɪ Dᴇᴍᴏɴ-sʟᴀʏᴇʀ 3
